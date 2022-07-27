663f947818bb86554275ea04612c86b1ce24306b
Brett LoCicero <brettlocicero@gmail.com>
2022-07-27 19:24:33 -0400

    started work on hitbox

---
+++ Assets/Scripts/HitboxAfterDelay.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitboxAfterDelay : MonoBehaviour
+{
+    [SerializeField] Transform trueHitbox;
+    [SerializeField] float delay = 1f;
+    [SerializeField] LayerMask playerLayer;
+
+    void Start ()
+    {
+        Invoke("Hitbox", delay);
+    }
+
+    void Hitbox ()
+    {
+        Collider2D hit = Physics2D.OverlapBox(trueHitbox.position, trueHitbox.localScale, 0f, playerLayer);
+        
+        if (hit) 
+        {
+            PlayerInstance.instance.TakeDamage();
+        }
+    }
+}
